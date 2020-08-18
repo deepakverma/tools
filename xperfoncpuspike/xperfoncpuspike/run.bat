551f62ce99c2b451c72c6b4d166dabb82cf4c75a
Deepak Verma <deepakv@microsoft.com>
2020-08-17 17:22:27 -0700

    remove xperfoncpuspike

--- xperfoncpuspike/xperfoncpuspike/run.bat
+++
@@ -1,4 +0,0 @@
-﻿xperf.exe -start AMXperf -on e4b70372-261f-4c54-8fa6-a5a7914d73da+cfeb0608-330e-4410-b00d-56d8da9986e6+8e92deef-5e17-413b-b927-59b2f06a3cfc+751ef305-6c6e-4fed-b847-02ef79d26aef+0A002690-3839-4E3A-B3B6-96D8DF868D99  -f amxperftrace.etl
-timeout 20
-xperf.exe -stop -stop AMXperf -d Merged.etl
-echo "done"
--- xperfoncpuspike/xperfoncpuspike/setup.bat
+++
@@ -1,1 +0,0 @@
-﻿xperf.exe -on Base+Diag+Latency+FileIO+DISPATCHER+REGISTRY+PERF_COUNTER -stackWalk %STACK% -f Kernel.etl -BufferSize 1024 2> out.txt
